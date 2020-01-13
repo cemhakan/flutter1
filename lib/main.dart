22a0e6d38dba4046ed5f41d84db561c0e766b014
cemhakan <throughthehourglass@hotmail.com>
2020-01-13 18:47:25 +0300

    cm4

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Deneme Projesi2'),
+      home: MyHomePage(title: 'Flutter Deneme Projesi3'),
     );
   }
 }
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter--;
     });
   }
 
